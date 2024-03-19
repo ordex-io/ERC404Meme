8766495d40533b89fc5bb179dbb6eb10635f3577
NanezX <victor.nanezj@gmail.com>
2024-03-19 04:12:06 -0400

    initial ERC404 base storage lib

---
+++ contracts/ERC404/base/ERC404BaseStorage.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library ERC404BaseStorage {
+    struct Layout {
+        // TODO: Make as Public access
+        uint256 totalSupply;
+        // TODO: Make as Public access
+        uint256 minted;
+        // TODO: Make as Public access
+        mapping(address => uint256) balanceOf;
+        // TODO: Make as Public access
+        mapping(address => mapping(address => uint256)) allowance;
+        // TODO: Make as Public access
+        mapping(uint256 => address) getApproved;
+        // TODO: Make as Public access
+        mapping(address => mapping(address => bool)) isApprovedForAll;
+        // TODO: Make as Public access
+        mapping(address => uint256) nonces;
+        // TODO: Make as Internal access
+        mapping(uint256 => uint256) _ownedData;
+        // TODO: Make as Internal access
+        mapping(address => uint256[]) _owned;
+        // TODO: Make as Internal access
+        mapping(address => bool) _erc721TransferExempt;
+    }
+
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256("ordex.contracts.storage.ERC404Base");
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
