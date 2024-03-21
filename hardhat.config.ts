6511b1eca55c6f13e9c04157a7c6266176fbcd17
NanezX <victor.nanezj@gmail.com>
2024-03-21 01:07:40 -0400

    added multple evm compiler versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+      },
+      { version: "0.4.18" },
+    ],
+  },
 };
 
 export default config;
