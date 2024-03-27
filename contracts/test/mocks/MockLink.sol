72bd1f51d2422b67aea772859a376bbf19a74234
NanezX <victor.nanezj@gmail.com>
2024-03-26 22:00:50 -0400

    mock link contract for testing

---
+++ contracts/test/mocks/MockLink.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {MockLinkToken} from "@chainlink/contracts/src/v0.8/mocks/MockLinkToken.sol";
+
+contract MockLink is MockLinkToken {}
