e9292a5ccb421307597bd53a1fc6ab292b62d214
NanezX <victor.nanezj@gmail.com>
2024-03-27 18:04:03 -0400

    added random base public

--- contracts/random/base/RandomBase.sol
+++ contracts/random/base/RandomBase.sol
@@ -3,4 +3,30 @@ pragma solidity ^0.8.24;
 
 import {RandomBaseInternal} from "./RandomBaseInternal.sol";
 
-abstract contract RandomBase is RandomBaseInternal {}
+abstract contract RandomBase is RandomBaseInternal {
+    function getKeyHash() public view returns (bytes32) {
+        return _getKeyHash();
+    }
+
+    function getSubscriptionId() public view returns (uint64) {
+        return _getSubscriptionId();
+    }
+
+    function getRequestConfirmations() public view returns (uint16) {
+        return _getRequestConfirmations();
+    }
+
+    function getCallbackGasLimit() public view returns (uint32) {
+        return _getCallbackGasLimit();
+    }
+
+    function getNumWords() public view returns (uint32) {
+        return _getNumWords();
+    }
+
+    function getResponsesById(
+        uint256 _requestId_
+    ) public view returns (uint256[] memory) {
+        return _getResponsesById(_requestId_);
+    }
+}
