0ed041de6f4a852e2f1a8a4664dfa4a1c7d08f5b
NanezX <victor.nanezj@gmail.com>
2024-03-28 02:04:21 -0400

    helper to deploy VRF mock

---
+++ test/utils/deploy.ts
@@ -0,0 +1,7 @@
+import { ethers } from "hardhat";
+
+export async function deployVRFCoordinartorV2Mock() {
+  const factory = await ethers.getContractFactory("CoordinatorV2Mock");
+  const contract = await factory.deploy(100, 100);
+  return contract;
+}
