e877d9fa6c04a5c303bf4d2c57e302538dae23fd
NanezX <victor.nanezj@gmail.com>
2024-03-28 02:24:51 -0400

    remove todo resolved

--- contracts/ERC404/base/ERC404BaseStorage.sol
+++ contracts/ERC404/base/ERC404BaseStorage.sol
@@ -5,25 +5,17 @@ import {DoubleEndedQueue} from "ERC404/contracts/lib/DoubleEndedQueue.sol";
 
 library ERC404BaseStorage {
     struct Layout {
-        // TODO: Make as Public access
+        // Public access
         uint256 totalSupply;
-        // TODO: Make as Public access
         uint256 minted;
-        // TODO: Make as Public access
         mapping(address => uint256) balanceOf;
-        // TODO: Make as Public access
         mapping(address => mapping(address => uint256)) allowance;
-        // TODO: Make as Public access
         mapping(uint256 => address) getApproved;
-        // TODO: Make as Public access
         mapping(address => mapping(address => bool)) isApprovedForAll;
-        // TODO: Make as Public access
         mapping(address => uint256) nonces;
-        // TODO: Make as Internal access
+        //  Internal access
         mapping(uint256 => uint256) _ownedData;
-        // TODO: Make as Internal access
         mapping(address => uint256[]) _owned;
-        // TODO: Make as Internal access
         mapping(address => bool) _erc721TransferExempt;
         // Private
         DoubleEndedQueue.Uint256Deque _storedERC721Ids;
