cadac91608fe9dba5922b0e71c2e4481ba6fcc52
NanezX <victor.nanezj@gmail.com>
2024-03-31 08:14:27 -0400

    storage for the NFT

---
+++ contracts/NFT404Storage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library NFT404Storage {
+    struct Layout {
+        mapping(uint256 => uint256) countersById;
+        uint256 nftRevealCounter;
+    }
+
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256("ordex.contracts.storage.NFT404");
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
