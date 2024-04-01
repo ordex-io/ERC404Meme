a512df817ec36943d02a9fbf2f6cb8a641715059
NanezX <victor.nanezj@gmail.com>
2024-03-31 23:11:50 -0400

    reorg dna code

--- contracts/dna/DNA.sol
+++ contracts/dna/DNA.sol
@@ -1,6 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {DNABase} from "./base/DNABase.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {DNABaseStorage} from "./DNABaseStorage.sol";
 
-abstract contract DNA is DNABase {}
+struct DNAInitParams {
+    bytes32 schemaHash;
+    string[] variant_name;
+    uint256[] variant_count;
+}
+
+abstract contract DNA is Initializable {
+    function getDnaOf(uint256 id_) public view virtual returns (bytes32);
+
+    function decodeADN(uint256 id_) public view virtual returns (string memory);
+
+    function __DNABase_init(
+        DNAInitParams memory initParams_
+    ) internal onlyInitializing {
+        require(
+            initParams_.variant_name.length == initParams_.variant_count.length,
+            "MISMATCH_VARIANT"
+        );
+
+        DNABaseStorage.layout().schemaHash = initParams_.schemaHash;
+        DNABaseStorage.layout().variant_name = initParams_.variant_name;
+        DNABaseStorage.layout().variant_count = initParams_.variant_count;
+    }
+
+    function _getDnaOf(
+        uint256 id_,
+        uint256 counterPoint_
+    ) internal view virtual returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    id_,
+                    DNABaseStorage.layout().wordsByCounter[counterPoint_]
+                )
+            );
+    }
+}
