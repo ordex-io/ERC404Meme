118b4e751bb497ef0b40d8eb9629f19a07015713
NanezX <victor.nanezj@gmail.com>
2024-04-03 20:50:11 -0400

    tests against initialization

--- test/NFT404/initialization.test.ts
+++ test/NFT404/initialization.test.ts
@@ -3,15 +3,25 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { deployNFT404 } from "../utils";
 
 describe.only("NFT404", () => {
-  it("Initialize the contract", async () => {
+  it("should initialize the contract correctly", async () => {
     const { nft404, erc404Params } = await loadFixture(deployNFT404);
 
-    BigInt(erc404Params.maxTotalSupplyERC721);
-    const units = 10n ** BigInt(erc404Params.decimals);
-    const totalSupplyExpected =
-      BigInt(erc404Params.maxTotalSupplyERC721) * units;
+    expect(await nft404.units()).to.be.equals(
+      erc404Params.units,
+      "wrong units in the contract"
+    );
 
-    expect(await nft404.totalSupply()).to.be.equals(totalSupplyExpected);
+    expect(await nft404.totalSupply()).to.be.equals(
+      erc404Params.maxTotalSupplyERC20,
+      "wrong erc20 total supply"
+    );
+
+    expect(
+      await nft404.balanceOf(erc404Params.initialMintRecipient)
+    ).to.be.equals(
+      erc404Params.maxTotalSupplyERC20,
+      "wrong balance assigned to initial minter"
+    );
 
     expect(await nft404.erc721TotalSupply()).to.be.equals(
       0n,
