978f966882f3c4025625201a36f4e007a008613e
NanezX <victor.nanezj@gmail.com>
2024-04-04 23:54:05 -0400

    helper function to get and decode erc721 transfer events

--- test/utils/events.ts
+++ test/utils/events.ts
@@ -1,7 +1,8 @@
 import { ContractTransactionResponse, BaseContract } from "ethers";
+import { bytesToAddress } from "./manipulation";
 
 /**
- *
+ * Obtain the first event that match the given event name
  * @param tx transaction where event occurs
  * @param eventName name of event
  * @param contract contract object holding the address, filters, interface
@@ -37,3 +38,42 @@ export const getEventArgs = async (
     eventObj.topics
   ) as any;
 };
+
+/**
+ * Get the ERC721 Transfer events values from a transaction.
+ *
+ * Since the ERC404 contract hold ERC20 and ERC721, and their topics are the same,
+ * the parse always try to decode as ERC20. This function only take the ERC721 transfers
+ * @param tx -  Transaction that contain the ERC721 transfers
+ * @param contract - The contract to loop the address and that have the interface to decode ERC721 transfers
+ * @param contractAddressOverride - Optional address that possible emitted the contract
+ * @returns
+ */
+export async function getERC721TransfersEventsArgs(
+  tx: ContractTransactionResponse,
+  contract: BaseContract,
+  contractAddressOverride: string | null = null
+) {
+  const txReceipt = await tx.wait();
+  if (!txReceipt) {
+    throw new Error("Could not get the tx receipt");
+  }
+
+  const eventLogs = txReceipt.logs.filter((log_) => {
+    return (
+      log_.topics[0] === contract.filters["Transfer"]().fragment.topicHash &&
+      log_.data.length <= 2 && // This because ERC721 transfer does index every data on the event. So, data is 0x
+      (contractAddressOverride === null ||
+        log_.address === contractAddressOverride)
+    );
+  });
+
+  return eventLogs.map((log_) => {
+    return {
+      index: log_.index,
+      from: bytesToAddress(log_.topics[1]),
+      to: bytesToAddress(log_.topics[2]),
+      id: BigInt(log_.topics[3]),
+    };
+  });
+}
