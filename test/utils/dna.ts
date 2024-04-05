7d7d4aaaa1f64d68c776b12b9ed0526f7706163e
NanezX <victor.nanezj@gmail.com>
2024-04-05 01:41:44 -0400

    dna calculation test helper

--- test/utils/dna.ts
+++ test/utils/dna.ts
@@ -1,13 +1,25 @@
-import { AbiCoder, keccak256 } from "ethers";
+import { ethers } from "hardhat";
+import { solidityPackedKeccak256 } from "ethers";
 
 /**
  * Calculate the DNA to compare it.
+ *
+ * This is based on the given words and make abi.encodePacked and hash it with keccak256
  */
-export function calculateDNA(id_: BigInt, words_: Array<BigInt>): string {
-  const encoded = AbiCoder.defaultAbiCoder().encode(
-    ["uint256", "uint256[]"],
-    [id_, words_]
-  );
+export function calculateDNA(id_: BigInt, words_: Array<bigint>): string {
+  return solidityPackedKeccak256(["uint256", "uint256[]"], [id_, words_]);
+}
 
-  return keccak256(encoded);
+export async function getBlockHash(blockNumber: number | null = null) {
+  if (blockNumber === null) {
+    blockNumber = await ethers.provider.provider.getBlockNumber();
+  }
+
+  const block = await ethers.provider.provider.getBlock(blockNumber);
+
+  if (block && block.hash) {
+    return block.hash;
+  } else {
+    throw `Not block obtained`;
+  }
 }
