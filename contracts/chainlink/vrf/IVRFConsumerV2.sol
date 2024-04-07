0bb2b078cdac62a83d5e541e8598a6834629bb85
NanezX <victor.nanezj@gmail.com>
2024-04-07 03:57:42 -0400

    removed wrong chainlink folder

--- contracts/chainlink/vrf/IVRFConsumerV2.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IVRFConsumerV2 {
-    function rawFulfillRandomWords(
-        uint256 requestId,
-        uint256[] memory randomWords
-    ) external;
-}
