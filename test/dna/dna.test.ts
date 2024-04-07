b8326c22a6db3bb3e9757e63dbd995b2ba4cd807
NanezX <victor.nanezj@gmail.com>
2024-04-06 20:06:34 -0400

    dna test for facet

--- test/dna/dna.test.ts
+++ test/dna/dna.test.ts
@@ -1,16 +1,9 @@
 import { expect } from "chai";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import {
-  deployNFT404,
-  getERC721TransfersEventsArgs,
-  getBlockHash,
-  calculateDNA,
-  dnaToJsonString,
-  nonDuplicateDNA,
-} from "../utils";
+// import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { calculateDNA, dnaToJsonString, nonDuplicateDNA } from "../utils";
 import { ethers } from "hardhat";
 
-describe.only("DNA Tests", () => {
+describe("DNA Tests", () => {
   it("should generate the DNA bytes correctly for ID/words saved", async () => {
     const schema_hash = ethers.randomBytes(32);
     const variants_name = ["head", "hat", "background", "eyes"];
@@ -96,4 +89,46 @@ describe.only("DNA Tests", () => {
 
     expect(nonDuplicateDNA(allDnas), "there are duplicated DNA").to.be.true;
   });
+
+  it("should generate the JSON string correctly", async () => {
+    const schema_hash = ethers.randomBytes(32);
+    const variants_name = ["head", "hat", "background", "eyes"];
+
+    const factory = await ethers.getContractFactory("DNAMock");
+
+    let contract = await factory.deploy(schema_hash, variants_name);
+    await contract.waitForDeployment();
+
+    const id_0 = BigInt(ethers.concat([ethers.randomBytes(32)]));
+
+    const randomWords_0 = Array.from({ length: 5 }).map(() => {
+      return BigInt(ethers.concat([ethers.randomBytes(32)]));
+    });
+
+    await contract.setWordsForIDs([id_0], randomWords_0);
+
+    let expectedDNA_0 = calculateDNA(id_0, randomWords_0);
+
+    expect(await contract.dnaOf(id_0)).to.be.equals(expectedDNA_0);
+
+    // 5 variant counts for each variant (only example);
+    const variantsCounters = variants_name.map(() => 5n);
+
+    // Pre-calculate the dna decoded
+    const expectedJsonString = dnaToJsonString(
+      variants_name,
+      variantsCounters,
+      schema_hash,
+      expectedDNA_0
+    );
+
+    // From DNA contract
+    const dnaDecoded = await contract.dnaOfToJson(id_0, variantsCounters);
+
+    expect(expectedJsonString).to.be.equals(dnaDecoded);
+  });
+
+  it("should revert dnaOf if an ID does not have words");
+
+  it("should revert dnaOfToJson if an ID does not have words");
 });
