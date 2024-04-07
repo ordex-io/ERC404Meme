5576cc243d6e33372c5f6cef08d29cdc95160939
NanezX <victor.nanezj@gmail.com>
2024-04-07 03:23:53 -0400

    request params storage

---
+++ contracts/chainlink/automation/vrf/RequestParamsStorage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library RequestParamsStorage {
+    struct Layout {
+        bytes32 keyHash;
+        uint64 subscriptionId;
+        uint16 requestConfirmations;
+        uint32 callbackGasLimit;
+        uint32 numWords;
+    }
+
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256("ordex.contracts.storage.request.params");
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
