adba30c0a56343b0e62fa34ac0cb5694abba56b3
NanezX <victor.nanezj@gmail.com>
2024-04-13 03:05:39 -0400

    add diamond iface

--- contracts/diamond/IDiamondNFT404.sol
+++ contracts/diamond/IDiamondNFT404.sol
@@ -5,4 +5,6 @@ import {IAutomationVRF} from "../automation/vrf/IAutomationVRF.sol";
 import {IDNA} from "../dna/IDNA.sol";
 import {INFT404} from "../NFT404/INFT404.sol";
 
-interface IDiamondNFT404 is IAutomationVRF, IDNA, INFT404 {}
+import {ISolidStateDiamond} from "@solidstate/contracts/proxy/diamond/ISolidStateDiamond.sol";
+
+interface IDiamondNFT404 is ISolidStateDiamond, IAutomationVRF, IDNA, INFT404 {}
