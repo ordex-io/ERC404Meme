03df83317ee9a8f7fdb8d4a66b2e71f4b7111320
NanezX <victor.nanezj@gmail.com>
2024-04-15 03:59:23 -0400

    removing unused ifaces

--- contracts/diamond/Diamond.sol
+++ contracts/diamond/Diamond.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.24;
 
 import {SolidStateDiamond} from "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
-import {IAutomationBase} from "../automation/non-vrf/IAutomationNonVRF.sol";
-import {IVRFConsumerV2} from "../automation/vrf/IAutomationVRF.sol";
-import {IDNA} from "../dna/IDNA.sol";
-import {INFT404, IERC404} from "../NFT404/INFT404.sol";
 
 contract Diamond is SolidStateDiamond {
     constructor(
