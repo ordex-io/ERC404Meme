cc6b43cbbb39d624a40993869dd271472ccf3e55
NanezX <victor.nanezj@gmail.com>
2024-04-19 12:19:15 -0400

    create mint script

---
+++ script/calls/mint.ts
@@ -0,0 +1,36 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+
+  // Our PET404 contract address with the PET404 interface (plus the exposer for tests)
+  const diamondPet404Address = "0x36983711f9C4869F0B9BEb2Cf677814bb40d41c5";
+  const diamondPet404 = await ethers.getContractAt(
+    "IPET404Exposer",
+    diamondPet404Address
+  );
+
+  // The minimum amount required to get a NFT (404k tokens) multiplied per 10
+  const amount = (await diamondPet404.units()) * 10n;
+  const receiver = owner.address;
+
+  // Mint
+  console.log(`Minting ${amount} ERC20 tokens to "${receiver}"`);
+  const tx = await diamondPet404
+    .connect(owner)
+    ["mintERC20(address,uint256)"](receiver, amount);
+
+  console.log("Transaction hash: ", tx.hash);
+
+  await tx.wait();
+  console.log("Transaction completed");
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
