56c488f0d0804d037d8b2b4c1333cc73be0b6748
NanezX <victor.nanezj@gmail.com>
2024-05-13 23:23:54 -0400

    updated contract for tests

--- contracts/test/examples/ERC404Example.sol
+++ contracts/test/examples/ERC404Example.sol
@@ -16,7 +16,7 @@ contract ERC404Example is Ownable, ERC404 {
         uint256 maxTotalSupplyERC721_,
         address initialMintRecipient_
     ) {
-        __ERC404_init(name_, symbol_, decimals_, units_);
+        __ERC404_init(name_, symbol_, decimals_, units_, address(0));
 
         // Save the base URI
         ERC404Storage.setBaseUri(baseUri_);
