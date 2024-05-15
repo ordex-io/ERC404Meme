cc427ac7d471365fbbc8394231b041a8074ca2cb
NanezX <victor.nanezj@gmail.com>
2024-05-15 17:37:29 -0400

    coment

--- contracts/automation/AutomationBase.sol
+++ contracts/automation/AutomationBase.sol
@@ -6,11 +6,12 @@ import {AutomationBaseStorage} from "./AutomationBaseStorage.sol";
 import {IAutomationBase} from "./IAutomationBase.sol";
 
 abstract contract AutomationBase is IAutomationBase, SafeOwnable {
-    /// @param caller_ the address that will be performing the automation calls
+    /// @param caller_ The address that will be performing the automation calls
     function __AutomationBase_Init(address caller_) internal {
         AutomationBaseStorage.layout().automationRegistry = caller_;
     }
 
+    /// @param newCaller_ The new address that will be performing the automation calls
     function setCallerAddress(address newCaller_) public onlyOwner {
         AutomationBaseStorage.layout().automationRegistry = newCaller_;
     }
