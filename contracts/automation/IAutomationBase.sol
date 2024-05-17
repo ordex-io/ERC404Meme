1c9583f07c49c33762eab5ff021d766265858e94
NanezX <victor.nanezj@gmail.com>
2024-05-17 18:54:21 -0400

    Moving the error to the Automation Interface

--- contracts/automation/IAutomationBase.sol
+++ contracts/automation/IAutomationBase.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.24;
 import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 
 interface IAutomationBase is AutomationCompatibleInterface {
+    error NoAutomationRegister();
+
     event RevealCalled(uint256 requestId, uint256 block);
 
     event NftsRevealed(
