96ddd2f0d94b3e1f55072360a6a6bb7b9261da13
NanezX <victor.nanezj@gmail.com>
2024-05-17 18:34:32 -0400

    check Up Keep call helper function

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,1 +1,25 @@
+import { ethers } from "hardhat";
+import { JsonRpcProvider } from "ethers";
+import { IPET404Exposer } from "../../typechain-types";
+
+type checkUpKeppResponse = { upkeepNeeded: boolean; performData: string };
+
+export async function checkUpKeppCall(
+  contract_: IPET404Exposer,
+  provider_: typeof ethers.provider | JsonRpcProvider
+): Promise<checkUpKeppResponse> {
+  const result = await provider_.call({
+    to: await contract_.getAddress(),
+    data: contract_.interface.encodeFunctionData("checkUpkeep", ["0x"]),
+    from: ethers.ZeroAddress,
+  });
+
+  const decodedResult = contract_.interface.decodeFunctionResult(
+    "checkUpkeep",
+    result
+  );
+
+  return decodedResult.toObject() as checkUpKeppResponse;
+}
+
 export { deployFullPET404DiamondNonVrf, deployUniswapPool } from "./deploy";
