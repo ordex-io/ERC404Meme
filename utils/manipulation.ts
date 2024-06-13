62fd8088168a889f8181e86af9719f14aeac6393
NanezX <victor.nanezj@gmail.com>
2024-06-12 21:08:10 -0400

    findCreate2Address with options to receive initcode address or factory with args

--- utils/manipulation.ts
+++ utils/manipulation.ts
@@ -1,13 +1,25 @@
 import { ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 
+type InitCode = string;
+type ContractAndArgs = {
+  factoryContract: ContractFactory;
+  args?: any[];
+}
+
 export function bytesToAddress(bytes_: string): string {
   return "0x" + bytes_.slice(-40);
 }
 
-export async function findCreate2Address(factoryAddress: string, factoryContract: ContractFactory, args: any[] = []) {
-  const deployArgs  = factoryContract.interface.encodeDeploy(args);
-  const initCode = ethers.concat([factoryContract.bytecode, deployArgs]);
+export async function findCreate2Address(factoryAddress: string, options: InitCode | ContractAndArgs) {
+  let initCode = '';
+  if (typeof options === 'string') {
+    initCode = options;
+  } else {
+    const {factoryContract, args = []} = options;
+    const deployArgs  = factoryContract.interface.encodeDeploy(args);
+    initCode = ethers.concat([factoryContract.bytecode, deployArgs]);
+  }
 
   for (let i = 0n; i < ethers.MaxUint256; i++) {
     const salt = ethers.toBeHex(i, 32);
