f8de75f590d279420b242f9fc70f05ee03c1a842
NanezX <victor.nanezj@gmail.com>
2024-08-15 23:09:50 -0400

    wip: using a catch for a fn for hh error

--- test/utils/deploy.ts
+++ test/utils/deploy.ts
@@ -249,123 +249,149 @@ export async function deployFullPET404DiamondVrf() {
 }
 
 export async function deployUniswapPool() {
-  // Deploy Uniswap Factory
-  const uniswapFactory = await deployUniswapV3Factory();
+  const errors = [];
+  for (let i = 0; i < 10; i++) {
+    try {
+      // Deploy Uniswap Factory
+      const uniswapFactory = await deployUniswapV3Factory();
 
-  // WETH token
-  const weth = await deployWeth();
+      // WETH token
+      const weth = await deployWeth();
 
-  // Position manager
-  const positionManager = await deployNonfungiblePositionManager(
-    uniswapFactory,
-    weth
-  );
+      // Position manager
+      const positionManager = await deployNonfungiblePositionManager(
+        uniswapFactory,
+        weth
+      );
 
-  // Swap router
-  const swapRouter = await deploySwapRouter(uniswapFactory, weth);
+      // Swap router
+      const swapRouter = await deploySwapRouter(uniswapFactory, weth);
 
-  // ERC20 token to use to create the pool
-  const erc20Token = await deployERC20Token();
+      // ERC20 token to use to create the pool
+      const erc20Token = await deployERC20Token();
 
-  // PET404NonVRF Related contracts
-  const PET404ContractsData = await deployFullPET404DiamondNonVrf();
+      // PET404NonVRF Related contracts
+      const PET404ContractsData = await deployFullPET404DiamondNonVrf();
 
-  // Configuration for the pool
-  const token1Address = await erc20Token.getAddress(); // token0
-  const erc404Address = await PET404ContractsData.diamondContract.getAddress(); // token1
-  // (0.05, 0.3, 1, 0.01)
-  const fee = 0.3 * 10000;
-  // The recipient signer is the same signer that we used when deploying the PET404NonVRF (initialMintRecipient)
-  const [recipientSigner] = await ethers.getSigners();
+      // Configuration for the pool
+      const token1Address = await erc20Token.getAddress(); // token0
+      const erc404Address =
+        await PET404ContractsData.diamondContract.getAddress(); // token1
+      // (0.05, 0.3, 1, 0.01)
+      const fee = 0.3 * 10000;
+      // The recipient signer is the same signer that we used when deploying the PET404NonVRF (initialMintRecipient)
+      const [recipientSigner] = await ethers.getSigners();
 
-  // Create the pool
-  const poolAddress = await createPool(
-    uniswapFactory,
-    erc404Address,
-    token1Address,
-    fee
-  );
+      // Create the pool
+      const poolAddress = await createPool(
+        uniswapFactory,
+        erc404Address,
+        token1Address,
+        fee
+      );
 
-  // Set Uniswap addresses as transfer exemptions and special exemption so they don't mint
-  // NFTs for they own and behave correclty.
-  // This is hihgly recommend to be used with pools
-  await setAddressAsSpecialExempt(
-    PET404ContractsData.diamondContract,
-    PET404ContractsData.ownerSigner,
-    [
-      await positionManager.getAddress(),
-      await swapRouter.getAddress(),
-      poolAddress,
-    ]
-  );
+      // Set Uniswap addresses as transfer exemptions and special exemption so they don't mint
+      // NFTs for they own and behave correclty.
+      // This is hihgly recommend to be used with pools
+      await setAddressAsSpecialExempt(
+        PET404ContractsData.diamondContract,
+        PET404ContractsData.ownerSigner,
+        [
+          await positionManager.getAddress(),
+          await swapRouter.getAddress(),
+          poolAddress,
+        ]
+      );
 
-  // Config for initialization of the pool
-  const price = encodePriceSqrt(404000, 1);
+      // Config for initialization of the pool
+      const price = encodePriceSqrt(404000, 1);
 
-  // Initilize the pool (the signer can be anyone who want to initialize the pool
-  await initializePool(poolAddress, price, recipientSigner);
+      // Initilize the pool (the signer can be anyone who want to initialize the pool
+      await initializePool(poolAddress, price, recipientSigner);
 
-  // ADDING LIQUIDITY
+      // ADDING LIQUIDITY
 
-  // Config for adding liquidity
-  const mulLiquidity =
-    PET404ContractsData.facetsArgs.pet404.maxTotalSupplyERC721_;
-  const positMangAddr = await positionManager.getAddress();
-  const chainId = (await recipientSigner.provider.getNetwork()).chainId;
-  const token1Decimals = await erc20Token.decimals();
-  const erc404Decimals = await PET404ContractsData.diamondContract.decimals();
-  const amountErc404 =
-    mulLiquidity * (await PET404ContractsData.diamondContract.units());
-  const amountToken1 =
-    ethers.parseUnits(mulLiquidity.toString(), 18) * mulLiquidity;
+      // Config for adding liquidity
+      const mulLiquidity =
+        PET404ContractsData.facetsArgs.pet404.maxTotalSupplyERC721_;
+      const positMangAddr = await positionManager.getAddress();
+      const chainId = (await recipientSigner.provider.getNetwork()).chainId;
+      const token1Decimals = await erc20Token.decimals();
+      const erc404Decimals =
+        await PET404ContractsData.diamondContract.decimals();
+      const amountErc404 =
+        mulLiquidity * (await PET404ContractsData.diamondContract.units());
+      const amountToken1 =
+        ethers.parseUnits(mulLiquidity.toString(), 18) * mulLiquidity;
 
-  const bal = await erc20Token.balanceOf(recipientSigner.address);
-  if (amountToken1 > bal) {
-    await erc20Token.connect(recipientSigner).mint(amountToken1 - bal);
+      const bal = await erc20Token.balanceOf(recipientSigner.address);
+      if (amountToken1 > bal) {
+        await erc20Token.connect(recipientSigner).mint(amountToken1 - bal);
+      }
+
+      // Check balances of the signer for both tokens
+      await checkBalances(erc20Token, recipientSigner.address, amountToken1);
+      await checkBalances(
+        PET404ContractsData.diamondContract,
+        recipientSigner.address,
+        amountErc404
+      );
+
+      // Approve the tokens to be used b the position manager
+      await approveTokens(
+        erc20Token,
+        recipientSigner,
+        amountToken1,
+        positMangAddr
+      );
+      await approveTokens(
+        PET404ContractsData.diamondContract,
+        recipientSigner,
+        amountErc404,
+        positMangAddr
+      );
+
+      // Add liquidity to the pool
+      await addLiquidityToPool(
+        poolAddress,
+        recipientSigner,
+        chainId,
+        token1Decimals,
+        erc404Decimals,
+        token1Address,
+        erc404Address,
+        amountToken1,
+        amountErc404,
+        fee,
+        await positionManager.getAddress()
+      );
+
+      return {
+        PET404ContractsData,
+        Uniswap: {
+          uniswapFactory,
+          weth,
+          positionManager,
+          swapRouter,
+          erc20Token,
+          fee,
+          poolAddress,
+        },
+      };
+    } catch (error) {
+      // NOTE: There is an issue here, we get 'STF' error which is an Uniswap error for `safeTransferFrom` library.
+      // For some reason is failing sometimes. It is not an issue with the deploy function, missing approves or a wrong
+      // value because if we re-order some test cases, the function pass. I guess is some issue with the HH node and
+      // an error with multiple instances deployed one after other. Until that, we try to force the deployment
+      // If the whole force fail, then something else is happening. We should be refactoring this an try to really figure
+      // out what is happening.
+      //
+      // This errors array is to log if at the end everything fail
+      errors.push(error!.toString());
+    }
   }
-
-  // Check balances of the signer for both tokens
-  await checkBalances(erc20Token, recipientSigner.address, amountToken1);
-  await checkBalances(
-    PET404ContractsData.diamondContract,
-    recipientSigner.address,
-    amountErc404
+  console.log(JSON.stringify(errors, null, 2));
+  throw new Error(
+    "An error happened. RUn again the tests, this is just a workaround for the uniswap issue that sometimes happen. Check the test logs"
   );
-
-  // Approve the tokens to be used b the position manager
-  await approveTokens(erc20Token, recipientSigner, amountToken1, positMangAddr);
-  await approveTokens(
-    PET404ContractsData.diamondContract,
-    recipientSigner,
-    amountErc404,
-    positMangAddr
-  );
-
-  // Add liquidity to the pool
-  await addLiquidityToPool(
-    poolAddress,
-    recipientSigner,
-    chainId,
-    token1Decimals,
-    erc404Decimals,
-    token1Address,
-    erc404Address,
-    amountToken1,
-    amountErc404,
-    fee,
-    await positionManager.getAddress()
-  );
-
-  return {
-    PET404ContractsData,
-    Uniswap: {
-      uniswapFactory,
-      weth,
-      positionManager,
-      swapRouter,
-      erc20Token,
-      fee,
-      poolAddress,
-    },
-  };
 }
